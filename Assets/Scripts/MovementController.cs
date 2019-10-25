c443a06ad2892a885e81c228c48d32cadc0c6fb8
Pablo Suarez <pa.suarezm@uniandes.edu.co>
2019-10-24 23:18:34 -0500

    More state machines

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    //Atributes for movement
+    public float movementSpeed = 3.0f;
+    Vector2 movement = new Vector2();
+    Rigidbody2D rb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the atribute directly from the GameObject
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Empty for now
+    }
+
+    void FixedUpdate()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+
+        rb2D.velocity = movement * movementSpeed;
+    }
+}
