0c1eb15cec1d66d5b5e47d94cb1ca5bf1ee756ef
Pablo Suarez <pa.suarezm@uniandes.edu.co>
2019-10-25 08:43:33 -0500

    Even more state machines

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,31 +4,74 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    //Atributes for movement
+    //Movement
     public float movementSpeed = 3.0f;
     Vector2 movement = new Vector2();
     Rigidbody2D rb2D;
 
+    //Animation
+    Animator animator;
+    string animationState = "AnimationState";
+    
+    //CharStates is shorthand for "Character States"
+    enum CharStates
+    {
+        walkEast = 1,
+        walkSouth = 2,
+        walkWest = 3,
+        walkNorth = 4,
+        idleSouth = 5
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        //Get the atribute directly from the GameObject
+        //Get the atributes directly from the GameObject
+        animator = GetComponent<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Empty for now
+        UpdateState();
     }
 
     void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    private void MoveCharacter()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         movement.Normalize();
-
         rb2D.velocity = movement * movementSpeed;
+    }
+
+    private void UpdateState()
+    {
+        if(movement.x > 0)
+        {
+            animator.SetInteger(animationState, (int) CharStates.walkEast);
+        }
+        else if(movement.x < 0)
+        {
+            animator.SetInteger(animationState, (int) CharStates.walkWest);
+        }
+        else if(movement.y > 0)
+        {
+            animator.SetInteger(animationState, (int) CharStates.walkNorth);
+        }
+        else if(movement.y < 0)
+        {
+            animator.SetInteger(animationState, (int) CharStates.walkSouth);
+        }
+        else
+        {
+            animator.SetInteger(animationState, (int) CharStates.idleSouth);
+        }
     }
 }
